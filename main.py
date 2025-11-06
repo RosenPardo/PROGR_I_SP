1fcb97a6bf8fd99bd59ce8413ca22a7e5ab02c99
Rosendo Pardo <rosen.pardox@gmail.com>
2025-11-05 23:05:02 -0300

    Se agrega módulo sudoku al main y se mapea el cuadrado "A1"

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import pygame as pg
+import random, pygame as pg
 from funciones.configuracion import *
+from funciones.sudoku import *
 
+
+numero_aleatorio = random.randint(1, 9)
+print(numero_aleatorio)
 
 pantalla = iniciar_juego()
+fuente = pg.font.SysFont(None, 80)
 
 dibujar_grilla(pantalla)
+
+
 
 while True:
 
@@ -12,12 +19,14 @@ while True:
         if evento.type == pg.QUIT:
             pg.quit()
             exit()
+        elif evento.type == pg.MOUSEBUTTONDOWN:
+            x, y = evento.pos
+            if x >= 52 and x <= 122:  # Verifica si la coordenada X está dentro del rango
+                if y >= 52 and y <= 122:  # Verifica si la coordenada Y está dentro del rango
+                    print(evento.pos) # Imprime si se hace clic dentro del cuadrado blanco A1
 
     pg.display.flip()
 
 
 
-#comentario de prueba para git
-
-
-# asdsad
+#Fuentes de windows: %USERPROFILE%\AppData\Local\Microsoft\Windows\Fonts
